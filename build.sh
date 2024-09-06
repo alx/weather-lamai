5750996e49a337784a1fdaf63d2b72b0cdd488e3
Alexandre Girard Davila <git@alexgirard.com>
2024-09-06 14:52:33 +0700

    fix: image links

--- build.sh
+++ build.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
+
+wget -O ./public/radar.gif https://weather.tmd.go.th/cmp/cmp240_latest.gif
+wget -O ./public/cmp1.gif https://weather.tmd.go.th/cmp/cmp1.gif
+wget -O ./public/cmp1.gif https://weather.tmd.go.th/cmp/cmp2.gif
+wget -O ./public/cmp3.gif https://weather.tmd.go.th/cmp/cmp3.gif
+wget -O ./public/cmp4.gif https://weather.tmd.go.th/cmp/cmp4.gif
+wget -O ./public/cmp5.gif https://weather.tmd.go.th/cmp/cmp5.gif
+wget -O ./public/cmp6.gif https://weather.tmd.go.th/cmp/cmp6.gif
+
 emacs -Q --script build-site.el
