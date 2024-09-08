70a60250850c050f766c83bc515c8263768b4aeb
Alexandre Girard Davila <git@alexgirard.com>
2024-09-08 08:08:33 +0700

    build: use imagemagick 6

--- build-imagemagick7.sh
+++ build-imagemagick7.sh
@@ -5,8 +5,8 @@
 RADAR_URL="https://data.rainviewer.com/images/THTP3/"
 LATEST_SCAN=`curl -0 ${RADAR_URL}0_products.json | jq -r ".products[0].scans | last | .name"`
 wget -O ./public/THTP3_source.gif ${RADAR_URL}${LATEST_SCAN/_2_map.png/_0_source.gif}
-convert ./public/THTP3_source.gif -crop 614x614+0+0 +repage -resize x614 +repage ./public/THTP3.webp
-convert ./public/THTP3.webp -resize 450x450 -background Black -gravity center -extent 450x450 ./public/THTP3.png
+magick ./public/THTP3_source.gif -crop 614x614+0+0 +repage -resize x614 +repage ./public/THTP3.webp
+magick ./public/THTP3.webp -resize 450x450 -background Black -gravity center -extent 450x450 ./public/THTP3.png
 rm ./public/THTP3_source.gif
 
 # THKT2 radar
@@ -14,8 +14,8 @@ rm ./public/THTP3_source.gif
 RADAR_URL="https://data.rainviewer.com/images/THKT2/"
 LATEST_SCAN=`curl -0 ${RADAR_URL}0_products.json | jq -r ".products[0].scans | last | .name"`
 wget -O ./public/THKT2_source.jpeg ${RADAR_URL}${LATEST_SCAN/_2_map.png/_0_source.jpeg}
-convert source.jpeg -crop 756x787+43+0 +repage -resize 756x787 +repage ./public/THKT2.webp
-convert ./public/THKT2.webp -resize 450x450 -background Black -gravity center -extent 450x450 ./public/THKT2.png
+magick source.jpeg -crop 756x787+43+0 +repage -resize 756x787 +repage ./public/THKT2.webp
+magick ./public/THKT2.webp -resize 450x450 -background Black -gravity center -extent 450x450 ./public/THKT2.png
 rm ./public/THKT2_source.jpeg
 
 # THMP3 radar
@@ -23,8 +23,8 @@ rm ./public/THKT2_source.jpeg
 RADAR_URL="https://data.rainviewer.com/images/THMP3/"
 LATEST_SCAN=`curl -0 ${RADAR_URL}0_products.json | jq -r ".products[0].scans | last | .name"`
 wget -O ./public/THMP3_source.gif ${RADAR_URL}${LATEST_SCAN/_2_map.png/_0_source.gif}
-convert ./public/THMP3_source.gif -crop 767x786+32+0 +repage -resize 767x786 +repage ./public/THMP3.webp
-convert ./public/THMP3.webp -resize 450x450 -background Black -gravity center -extent 450x450 ./public/THMP3.png
+magick ./public/THMP3_source.gif -crop 767x786+32+0 +repage -resize 767x786 +repage ./public/THMP3.webp
+magick ./public/THMP3.webp -resize 450x450 -background Black -gravity center -extent 450x450 ./public/THMP3.png
 rm ./public/THMP3_source.gif
 
 # THNN radar
@@ -32,8 +32,8 @@ rm ./public/THMP3_source.gif
 RADAR_URL="https://data.rainviewer.com/images/THNN/"
 LATEST_SCAN=`curl -0 ${RADAR_URL}0_products.json | jq -r ".products[0].scans | last | .name"`
 wget -O ./public/THNN_source.webp ${RADAR_URL}${LATEST_SCAN/_2_map.png/_0_source.webp}
-convert ./public/THNN_source.webp -crop 607x607+0+0 +repage -resize x614 +repage ./public/THNN.webp
-convert ./public/THNN.webp -resize 450x450 -background Black -gravity center -extent 450x450 ./public/THNN.png
+magick ./public/THNN_source.webp -crop 607x607+0+0 +repage -resize x614 +repage ./public/THNN.webp
+magick ./public/THNN.webp -resize 450x450 -background Black -gravity center -extent 450x450 ./public/THNN.png
 rm ./public/THNN_source.webp
 
 # THTR radar
@@ -41,8 +41,8 @@ rm ./public/THNN_source.webp
 RADAR_URL="https://data.rainviewer.com/images/THTR/"
 LATEST_SCAN=`curl -0 ${RADAR_URL}0_products.json | jq -r ".products[0].scans | last | .name"`
 wget -O ./public/THTR_source.webp ${RADAR_URL}${LATEST_SCAN/_2_map.png/_0_source.webp}
-convert ./public/THTR_source.webp -crop 799x799+0+0 +repage -resize x799 +repage ./public/THTR.webp
-convert ./public/THTR.webp -resize 450x450 -background Black -gravity center -extent 450x450 ./public/THTR.png
+magick ./public/THTR_source.webp -crop 799x799+0+0 +repage -resize x799 +repage ./public/THTR.webp
+magick ./public/THTR.webp -resize 450x450 -background Black -gravity center -extent 450x450 ./public/THTR.png
 rm ./public/THTR_source.webp $
 
 emacs -Q --script build-site.el
