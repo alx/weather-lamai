75f6e36dbad417bee19e62d1333e729ec232fc6d
Alexandre Girard <git@alexgirard.com>
2024-10-05 16:57:57 +0700

    fix: animated gif, update every 15min

--- build.sh
+++ build.sh
@@ -2,72 +2,82 @@
 
 convert_latest_radar_image () {
 
-    # rotate image files (from oldest to newest)
-    if [ -f "./public/${RADAR_NAME}_4.png" ]; then
-        rm ./public/${RADAR_NAME}_4.png
-    fi
-    mv ./public/${RADAR_NAME}_3.png ./public/${RADAR_NAME}_4.png
-    mv ./public/${RADAR_NAME}_2.png ./public/${RADAR_NAME}_3.png
-    mv ./public/${RADAR_NAME}_1.png ./public/${RADAR_NAME}_2.png
-    mv ./public/${RADAR_NAME}_0.png ./public/${RADAR_NAME}_1.png
-    cp ./public/${RADAR_NAME}.png   ./public/${RADAR_NAME}_0.png
-
-    # create animated GIF
-    convert -delay 100 -loop 0 ./public/${RADAR_NAME}_*.png ./public/${RADAR_NAME}_animated.gif
-
     RADAR_NAME=$1
     FILE_EXTENSION=$2
-
     CROP_DIMENSION=$3
     RESIZE_DIMENSION=$4
+    FRAME_DELAY=$5
 
     RADAR_URL="https://data.rainviewer.com/images/${RADAR_NAME}/"
 
-    DEST_SOURCE_FILE="./public/${RADAR_NAME}_source.${FILE_EXTENSION}"
-    DEST_DONE_FILE="./public/${RADAR_NAME}.png"
+    DEST_DIR="./public/${RADAR_NAME}_frames/"
+    DEST_DONE_FILE="./public/${RADAR_NAME}.gif"
 
-    LATEST_SCAN=`curl -0 ${RADAR_URL}0_products.json | jq -r ".products[0].scans | last | .name"`
-    IMG_URL_FROM_JSON=${RADAR_URL}${LATEST_SCAN/_2_map.png/_0_source.${FILE_EXTENSION}}
+    # Create the destination directory for frames if it doesn't exist
+    mkdir -p ${DEST_DIR}
 
-    wget -O ${DEST_SOURCE_FILE} ${IMG_URL_FROM_JSON}
+    # Get the latest 5 scans
+    LATEST_SCANS=$(curl -s ${RADAR_URL}0_products.json | jq -r ".products[0].scans[-5:] | .[].name")
 
-    # Crop and resize radar image on file
-    convert ${DEST_SOURCE_FILE}             \
-        -crop ${CROP_DIMENSION} +repage     \
-        -resize ${RESIZE_DIMENSION} +repage \
-        ${DEST_DONE_FILE}
+    COUNTER=0
+    for SCAN in ${LATEST_SCANS}; do
+        # Generate image URLs
+        IMG_URL_FROM_JSON=${RADAR_URL}${SCAN/_2_map.png/_0_source.${FILE_EXTENSION}}
+        echo $IMG_URL_FROM_JSON
 
-    # Resize images
-    # to 450x450
-    convert ${DEST_DONE_FILE} \
-        -resize 450x450       \
-        -background Black     \
-        -gravity center       \
-        -extent 450x450       \
-        ${DEST_DONE_FILE}
+        # Set destination file for each frame
+        DEST_SOURCE_FILE="${DEST_DIR}${RADAR_NAME}_source_${COUNTER}.${FILE_EXTENSION}"
+        DEST_DONE_FRAME="${DEST_DIR}${RADAR_NAME}_frame_${COUNTER}.png"
 
-    rm ${DEST_SOURCE_FILE}
+        # Download the image
+        wget -O ${DEST_SOURCE_FILE} ${IMG_URL_FROM_JSON}
+
+        # Crop and resize each radar image
+        convert ${DEST_SOURCE_FILE}      \
+            -crop ${CROP_DIMENSION} +repage     \
+            -resize ${RESIZE_DIMENSION} +repage \
+            ${DEST_DONE_FRAME}
+
+        # Resize to 450x450 and center
+        convert ${DEST_DONE_FRAME} \
+            -resize 450x450       \
+            -background Black     \
+            -gravity center       \
+            -extent 450x450       \
+            ${DEST_DONE_FRAME}
+
+        # Remove the source file to clean up
+        rm ${DEST_SOURCE_FILE}
+
+        COUNTER=$((COUNTER+1))
+    done
+    # Create an animated GIF from the frames
+    convert -delay ${FRAME_DELAY} -loop 0 ${DEST_DIR}*.png ${DEST_DONE_FILE}
+
+    # Clean up the frames directory
+    rm -rf ${DEST_DIR}
 
 }
 
 # THTP3 radar
 # https://data.rainviewer.com/images/THTP3/
-convert_latest_radar_image "THTP3" "gif" "614x614+0+0" "614x614"
+convert_latest_radar_image "THTP3" "gif" "614x614+0+0" "614x614" 100
 
 # THKT2 radar
 # https://data.rainviewer.com/images/THKT2/
-convert_latest_radar_image "THKT2" "jpeg" "756x787+43+0" "756x787"
+convert_latest_radar_image "THKT2" "jpeg" "756x787+43+0" "756x787" 100
 
 # THMP3 radar
 # https://data.rainviewer.com/images/THMP3/
-convert_latest_radar_image "THMP3" "gif" "767x786+32+0" "767x786"
+convert_latest_radar_image "THMP3" "gif" "767x786+32+0" "767x786" 100
 
 # THNN radar
 # https://data.rainviewer.com/images/THNN/
-convert_latest_radar_image "THNN" "webp" "607x607+0+0" "614x614"
+convert_latest_radar_image "THNN" "webp" "607x607+0+0" "614x614" 100
 
 # THTR radar
 # https://data.rainviewer.com/images/THTR/
-convert_latest_radar_image "THTR" "webp" "799x799+0+0" "799x799"
+convert_latest_radar_image "THTR" "webp" "799x799+0+0" "799x799" 100
 
+# org content into website
 emacs -Q --script build-site.el
